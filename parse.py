e192acff9dbe7104ed6bc508b02eefe32b686a32
sachin1092 <me@sachinshinde.com>
2015-02-12 23:48:14 +0530

    episode url

--- parse.py
+++ parse.py
@@ -17,3 +17,9 @@ page = requests.get(series_url)
 tree = html.fromstring(page.text)
 episode_url = base_url + tree.xpath('//*[@id="left"]/div/ul/li[1]/a')[0].values()[0]
 print episode_url
+
+episode_page = requests.get(episode_url)
+
+episode_tree = html.fromstring(episode_page.text)
+video_url = episode_tree.xpath('//*[@id="link_56963690"]/td[2]/a')[0].values()[1]
+print video_url
