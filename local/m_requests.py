e648f1ac8827bcf5af4cd1ee983a9aeb21e9a242
sachin1092 <me@sachinshinde.com>
2015-02-16 08:49:19 +0530

    added cookies support in my_requests

--- local/m_requests.py
+++ local/m_requests.py
@@ -3,5 +3,7 @@ import requests
 __author__ = 'sachin'
 
 
-def get(url):
+def get(url, cookies=None):
+    if cookies:
+        return requests.get("http://series-downloader.appspot.com/getURLResponse?url=" + url, cookies=cookies)
     return requests.get("http://series-downloader.appspot.com/getURLResponse?url=" + url)
