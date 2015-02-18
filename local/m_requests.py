f3637b169ca8030115290e2eb24196123712aa26
sachin1092 <me@sachinshinde.com>
2015-02-18 09:09:50 +0530

    testing headers

--- local/m_requests.py
+++ local/m_requests.py
@@ -3,7 +3,11 @@ import requests
 __author__ = 'sachin'
 
 
-def get(url, cookies=None):
-    if cookies:
+def get(url, cookies=None, headers=None):
+    if cookies and headers:
+        return requests.get("http://series-downloader.appspot.com/getURLResponse?url=" + url, cookies=cookies, headers=headers)
+    elif cookies:
         return requests.get("http://series-downloader.appspot.com/getURLResponse?url=" + url, cookies=cookies)
+    elif headers:
+        return requests.get("http://series-downloader.appspot.com/getURLResponse?url=" + url, headers=headers)
     return requests.get("http://series-downloader.appspot.com/getURLResponse?url=" + url)
