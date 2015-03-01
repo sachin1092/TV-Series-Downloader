25652b836ab5a1ac736f989d7e836c145a0c560d
sachin1092 <me@sachinshinde.com>
2015-03-01 15:17:09 +0530

    changing method params

--- local/series_extractor.py
+++ local/series_extractor.py
@@ -11,14 +11,15 @@ if __name__ == '__main__':
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 from lxml import html
-import requests
+import m_requests
+from time import sleep
 from logger import write_to_requester_log
 
 __author__ = 'sachin'
 
 import traceback
 
-def extract_episode_info(series, season, ep, skip_urls=None):
+def extract_episode_info(series, season, ep):
 
     write_to_requester_log("\n")
     write_to_requester_log("-" * 40)
@@ -46,25 +47,21 @@ def extract_episode_info(series, season, ep, skip_urls=None):
             video_tree.xpath('/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div/div/div/a')[0].values()[0]
         write_to_requester_log(gorilla_url)
 
-        download_resp = requests.get(
-            'http://my-youtube-dl.appspot.com/api/info?url=' + gorilla_url + '&flatten=True')
-    for vid_url in video_urls:
-        if skip_urls and vid_url in skip_urls:
-            continue
-        write_to_requester_log("*"*50)
-        write_to_requester_log("\n\n\n")
-        write_to_requester_log('requesting ' + base_url + vid_url)
         try:
-            downld_json = json.loads(requests.get('http://my-youtube-dl.appspot.com/api/info?url=' 
-                + base_url + vid_url + '&flatten=True').text)
-            if downld_json.get('videos') is not None:
-                return {'title': downld_json.get('videos')[0].get('title'), 
-                    'download_url': downld_json.get('videos')[0].get('url'),
-                    'url': vid_url}
+
+            download_resp = requests.get(
+                'http://my-youtube-dl.appspot.com/api/info?url=' + gorilla_url + '&flatten=True')        
+            
+            if download_resp.get('videos') is not None:
+                return {'title': download_resp.get('videos')[0].get('title'), 
+                    'download_url': download_resp.get('videos')[0].get('url'),
+                    'url': gorilla_url}
         except:
             traceback.print_exc()
     return {'error': 'no vid found'}
 
 if __name__ == '__main__':
+    series = raw_input("Enter series name to download: ")
+    season = raw_input("Enter season to download: ")
     episode = raw_input("Enter episode to download: ")
-    write_to_requester_log(extract_episode_info(episode))
+    write_to_requester_log(extract_episode_info(series, season, episode))
