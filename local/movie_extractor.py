2c713dd066541bf54d9610937dd56f55d92d6fec
sachin1092 <me@sachinshinde.com>
2015-03-07 14:24:02 +0530

    url extraction done from youtube-dl

--- local/movie_extractor.py
+++ local/movie_extractor.py
@@ -14,74 +14,11 @@ from logger import write_to_requester_log
 
 __author__ = 'sachin'
 
-#
-# def extractor(index):
-#     # print "Requesting: " + "http://urgrove.com/?s=" + movie
-#     # movie_search = requests.get("http://urgrove.com/?s=" + movie)
-#     # movie_search_tree = html.fromstring(movie_search.text)
-#     # try:
-#     # print movie_search_tree.xpath('//*[@id="content"]/div[1]/div[2]/a')[0].values()
-#     # except IndexError:
-#     # write_to_requester_log("No videos for this epiosde")
-#
-#
-#     ses = lt.session()
-#     ses.listen_on(6881, 6891)
-#     params = {
-#         'save_path': os.getcwd(),
-#         'storage_mode': lt.storage_mode_t(2),
-#         'paused': False,
-#         'auto_managed': True,
-#         'duplicate_is_error': True}
-#     link = "magnet:?xt=urn:btih:56169D27E03749D8288A899D9C6A123EC57AC7EE&dn=" \
-#            "ccleaner+5+03+5128+incl+business+technician+professional+" \
-#            "edition+crack+atom&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=" \
-#            "udp%3A%2F%2Fopen.demonii.com%3A1337"
-#     handle = lt.add_magnet_uri(ses, link, params)
-#     ses.start_dht()
-#
-#     print 'downloading metadata...'
-#     while not handle.has_metadata():
-#         time.sleep(1)
-#
-#     print dir(handle)
-#     print "File priorities"
-#     file_priorities = handle.file_priorities()
-#     print file_priorities
-#     file_priority = [0 for n in range(len(file_priorities))]
-#     file_priority[index] = 1
-#     print handle.prioritize_files(file_priority)
-#     print handle.file_priorities()
-#     print "piece priorities"
-#     print handle.piece_priorities()
-#     file_piece_count = len(filter(lambda x: x == 1, handle.piece_priorities()))
-#     print "File " + str(index) + " has " + str(file_piece_count) + " pieces."
-#     print handle.save_path()
-#     # print handle.prioritize_pieces({1: 0})
-#     # print handle.piece_priorities()
-#     #
-#     #
-#
-#     # print 'got metadata, starting torrent download...'
-#     # while handle.status().state != lt.torrent_status.seeding:
-#     # s = handle.status()
-#     #     state_str = ['queued', 'checking', 'downloading metadata',
-#     #                  'downloading', 'finished', 'seeding', 'allocating']
-#     #     print '%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s %d.3' % \
-#     #           (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000,
-#     #            s.num_peers, state_str[s.state], s.total_download / 1000000)
-#     #     time.sleep(5)
-#
-#
-# def download(handle):
-#     while handle.status().state != lt.torrent_status.seeding:
-#         s = handle.status()
-#         state_str = ['queued', 'checking', 'downloading metadata',
-#                      'downloading', 'finished', 'seeding', 'allocating']
-#         print '%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s %d.3' % \
-#               (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000,
-#                s.num_peers, state_str[s.state], s.total_download / 1000000)
-#         time.sleep(5)
+import traceback
+
+def get_redirected_url(url):
+    req = requests.get(url)
+    return req.url
 
 
 def extract_movie_info(movie):
@@ -100,14 +37,23 @@ def extract_movie_info(movie):
     print len(number_of_urls)
     video_urls = []
     try:
-        for i in xrange(1, len(number_of_urls) + 1):
+        for i in xrange(1, len(number_of_urls)):
             print i
             vid_xpath = '/html/body/div[2]/div[2]/div[5]/div[3]/table[%d]/tbody/tr/td[2]/strong/a' % i
             video_urls.append(video_page.xpath(vid_xpath)[0].values()[0])
     except:
-        import traceback
         traceback.print_exc()
     print video_urls
+    for vid_url in video_urls:
+        print "*"*50
+        print "\n\n\n"
+        print 'requesting', base_url + vid_url
+        # try:
+        #     print get_redirected_url(base_url + vid_url)
+        # except:
+        #     traceback.print_exc()
+        print requests.get(
+            'http://my-youtube-dl.appspot.com/api/info?url=' + base_url + vid_url + '&flatten=True').text
 
 
 
