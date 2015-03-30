c9709debc3fa872af986170564e40ccc02b2f919
sachin1092 <me@sachinshinde.com>
2015-03-30 15:17:28 +0530

    merged one_piece into series

--- local/series_extractor.py
+++ local/series_extractor.py
@@ -1,3 +1,7 @@
+__author__ = 'sachin'
+
+base_url = 'http://www.watchfree.to'
+
 import logging
 import os
 import string
@@ -6,82 +10,59 @@ import json
 import re
 
 if __name__ == '__main__':
-    from os import sys, path
+	from os import sys, path
 
-    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+	sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 from lxml import html
-import m_requests
 import requests
-from time import sleep
+import m_requests
 from logger import write_to_requester_log
+from py.config import ConfigReader
 
 __author__ = 'sachin'
 
 import traceback
 
 
-def extract_episode_info(series, season, ep, skip_urls=None):
-    
-    base_url = 'http://watch-tv-series.to'
+def extract_episode_info(season, ep, url, skip_urls=None, quality='[DVD]'):
+	write_to_requester_log(url, False)
+	video_request = m_requests.get(url)
 
-    write_to_requester_log("\n")
-    write_to_requester_log("-" * 40)
-
-    write_to_requester_log("Will request download of " + series + " Season " + str(season) + " Episode " + str(ep))
-    episode_page = m_requests.get("http://watch-tv-series.to/episode/"
-                                  + series + "_s" + str(season) + "_e" + str(ep) + ".html")
-    write_to_requester_log("Requesting URL http://watch-tv-series.to/episode/"
-                           + series + "_s" + str(season) + "_e" + str(ep) + ".html")
-
-    if "Um, Where did the page go?" in episode_page.content:
-        return False
-
-    sleep(1)
-    episode_tree = html.fromstring(episode_page.text)
-    number_of_urls = re.findall('</tr>', episode_page.text)
-    for i in xrange(2, len(number_of_urls)):
-        try:
-            xpath = '//*[@id="myTable"]/tbody/tr[%d]/td[2]/a' % i
-            video_url = base_url + episode_tree.xpath(xpath)[0].values()[1]
-        except IndexError:
-            write_to_requester_log("No videos for this episode")
-        else:
-            write_to_requester_log(video_url)
-
-            sleep(1)
-
-            try:
-
-                video_page = m_requests.get(video_url)
-                video_tree = html.fromstring(video_page.text)
-
-                gorilla_url = \
-                    video_tree.xpath('/html/body/div[2]/div[2]/div/div[2]/div/div/div/div/div/div/div/a')[0].values()[0]
-                    
-                write_to_requester_log(gorilla_url)
-
-                if skip_urls and gorilla_url in skip_urls:
-                    write_to_requester_log('skipping url: ' + gorilla_url)
-                    continue
-
-                download_resp = json.loads(requests.get(
-                    'http://my-youtube-dl.appspot.com/api/info?url=' + gorilla_url + '&flatten=True').text)
-
-                if 'error' in download_resp.keys():
-                    write_to_requester_log(download_resp)
-
-                if download_resp.get('videos') is not None:
-                    return {'title': download_resp.get('videos')[0].get('title'),
-                            'download_url': download_resp.get('videos')[0].get('url'),
-                            'url': gorilla_url, 'ext': download_resp.get('videos')[0].get('ext')}
-            except:
-                traceback.print_exc()
-    return {'error': 'no vid found'}
+	video_page = html.fromstring(video_request.text)
+	number_of_urls = re.findall('</table>', video_request.text)
+	video_urls = []
+	try:
+	    for i in xrange(1, len(number_of_urls)):
+	        vid_xpath = '/html/body/div[2]/div[2]/div[4]/div[3]/table[%d]/tbody/tr/td[2]/strong/a' % i
+	        vid_quality = video_page.xpath(
+	            '/html/body/div[2]/div[2]/div[4]/div[3]/table[%d]/tbody/tr/td[1]/div' % i)[0].text
+	        if vid_quality != quality:
+	            continue
+	        video_urls.append(video_page.xpath(vid_xpath)[0].values()[0])
+	except:
+		if not len(video_urls):
+			import traceback
+			traceback.print_exc()
+	for vid_url in video_urls:
+	    if skip_urls and vid_url in skip_urls:
+	        continue
+	    write_to_requester_log("\n\n\n", False)
+	    write_to_requester_log("-" * 50, False)
+	    write_to_requester_log('requesting ' + base_url + vid_url, False)
+	    try:
+	        download_json = json.loads(requests.get('http://my-youtube-dl.appspot.com/api/info?url='
+	                                                + base_url + vid_url + '&flatten=True').text)
+	        if download_json.get('videos') is not None:
+	            return {'title': download_json.get('videos')[0].get('title'),
+	                    'download_url': download_json.get('videos')[0].get('url'),
+	                    'url': vid_url, 'ext': download_json.get('videos')[0].get('ext')}
+	    except Exception, e:
+	        write_to_requester_log(e.message, False)
+	return {'error': 'No videos found'}
 
 
 if __name__ == '__main__':
-    series = raw_input("Enter series name to download: ")
-    season = raw_input("Enter season to download: ")
-    episode = raw_input("Enter episode to download: ")
-    write_to_requester_log(extract_episode_info(series, season, episode))
+	season = raw_input("Enter one piece season to download: ")
+	episode = raw_input("Enter one piece episode to download: ")
+	write_to_requester_log(extract_episode_info(season, episode))
--- local/series_requester.py
+++ local/series_requester.py
@@ -1,7 +1,9 @@
 import json
-import traceback
 from os.path import expanduser
-import re
+import traceback
+import requests
+import m_requests
+from lxml import html
 
 if __name__ == '__main__':
     from os import sys, path
@@ -12,118 +14,106 @@ from py.config import ConfigReader
 from series_extractor import extract_episode_info
 from logger import write_to_requester_log
 from local import direct_download, subtitle_downloader
-import m_requests
-from lxml import html
 
-
-def download(series, season, episode):
-    urls_used = []
-    done = False
-
-    while not done:
-
-        if len(urls_used) > 40:
-            done = True
-
-        print "-" * 20
-        episode_info = extract_episode_info(series, season, episode, skip_urls=urls_used)
-
-        if episode_info is False:
-            return False
-
-        if "error" in episode_info.keys():
-            write_to_requester_log(episode_info.get("error"), True)
-            done = True
-            continue
-        title = episode_info.get('title')
-        download_url = episode_info.get('download_url')
-        urls_used.append(episode_info.get('url'))
-        ext = episode_info.get('ext')
-
-        print title
-        print download_url
-        print urls_used
-        print ext
-
-        try:
-            direct_download.divide_n_download(series + '_s' + str(season) + 'e' + str(episode), download_url, ext, (
-                'My-Downloads/Series-Downloads/' + series.replace('_', ' ').title().strip() + '/Season %d' % season))
-        except:
-            print "\n\n\nError:"
-            print '*' * 50
-            traceback.print_exc()
-            print '*' * 50
-            done = False
-        else:
-            subtitle_downloader.download_sub(title, (expanduser("~") +
-                                                     '/My-Downloads/Series-Downloads/'
-                                                     + series.replace('_', ' ').title() + '/Season %d' % season),
-                                             title)
-            done = True
-            configReader = ConfigReader()
-            config = configReader.get_settings_parser()
-            series_list = configReader.get_series_list()
-            series_list[series]['season'] = season
-            series_list[series]['episode'] = episode
-            config.set('Series', 'list', json.dumps(series_list))
-            with open('downloader.ini', 'wb') as configfile:
-                config.write(configfile)
-            return True
-
+base_url = 'http://www.watchfree.to'
 
 def check():
     config = ConfigReader()
     series_list = config.get_series_list()
-    print series_list
     for series in series_list.keys():
+        try:
 
-        print "\n\n"
+            all_downloaded = False
 
-        last_season = series_list[series].get('season')
-        last_episode = series_list[series].get('episode')
+            serie_url = get_series_url(series)
+            last_season = series_list[series].get('season')
+            last_episode = series_list[series].get('episode')
 
-        write_to_requester_log(
-            "Last Episode Downloaded of " + series + " is S" + str(last_season) + "E" + str(last_episode))
-        write_to_requester_log("Checking if new episode came")
+            write_to_requester_log(
+                "Last Episode Downloaded of " + series + " is S" + str(last_season) + "E" + str(last_episode))
+            write_to_requester_log("Checking if new episode came")
 
-        series_url = 'http://watch-tv-series.to' + '/serie/' + series
-        page = m_requests.get(series_url)
-        tree = html.fromstring(page.text)
-        episode_url = 'http://watch-tv-series.to' + tree.xpath('//*[@id="left"]/div/ul/li[1]/a')[0].values()[0]
-        latest = re.search(series + '_s(\d{1,2})_e(\d{1,2})', episode_url)
-        season = int(latest.group(1))
-        episode = int(latest.group(2))
-        if last_season == season:
-            if last_episode < episode:
-                for ep in range(last_episode + 1, episode + 1):
-                    download(series, season, ep)
+            while not all_downloaded:
+                last_episode = last_episode + 1
+                series_url = base_url + serie_url + ("/season-%s-episode-%s" % (last_season, last_episode))
+                print series, ": ", series_url
+                video_page = m_requests.get(series_url)
+                # print video_page.text
+                if 'File not found.' in video_page.text:
+                    last_season = last_season + 1
+                    last_episode = 1
+                    series_url = base_url + series_url + ("/season-%s-episode-%s" % (last_season, last_episode))
+                    print series, ": ", series_url
+                    video_page = m_requests.get(series_url)
+                    # print video_page.text
+                    if 'File not found.' in video_page.text:
+                        print "Nothing to be done here."
+                        all_downloaded = True
 
-            elif last_episode > episode:
-                write_to_requester_log("Wrong config, Please check")
-            elif last_episode == episode:
-                write_to_requester_log("No new episodes")
-        elif last_season < season:
-            for n_season in range(last_season, season + 1):
-                if last_season == n_season:
-                    for ep in range(last_episode+1, 25):
+                urls_used = []
+                if not all_downloaded:
+                    done = False
+                    while not done:
+
+                        if len(urls_used) > 40:
+                            done = True
+
+                        print "-" * 20
+                        print "Extracting url for season %d episode %d" % (last_season, last_episode)
+                        episode_info = extract_episode_info(last_season, last_episode, series_url, skip_urls=urls_used)
+                        if "error" in episode_info.keys():
+                            write_to_requester_log(episode_info.get("error"), True)
+                            done = True
+                            continue
+                        title = episode_info.get('title')
+                        download_url = episode_info.get('download_url')
+                        urls_used.append(episode_info.get('url'))
+                        ext = episode_info.get('ext')
+
+                        print title
+                        print download_url
+                        print urls_used
+                        print ext
+
                         try:
-                            if not download(series, n_season, ep):
-                                break
-                        except Exception:
-                            break
-                else:
-                    for ep in range(1, episode + 1):
-                        if not download(series, n_season, ep):
-                            break
+                            direct_download.divide_n_download(series + '_s' + str(last_season) + 'e' + str(last_episode), download_url, ext, (
+                                'My-Downloads/Series-Downloads/' + series.replace('_', ' ').title().strip() + '/Season %d' % last_season))
+                        except:
+                            print "\n\n\nError:"
+                            print '*' * 50
+                            traceback.print_exc()
+                            print '*' * 50
+                            done = False
+                        else:
+                            subtitle_downloader.download_sub(title, (expanduser("~") +
+                                                     '/My-Downloads/Series-Downloads/'
+                                                     + series.replace('_', ' ').title() + '/Season %d' % season),
+                                             title)
+                            done = True
+                            configReader = ConfigReader()
+                            config = configReader.get_settings_parser()
+                            series_list = configReader.get_series_list()
+                            series_list[series]['season'] = season
+                            series_list[series]['episode'] = episode
+                            config.set('Series', 'list', json.dumps(series_list))
+                            with open('downloader.ini', 'wb') as configfile:
+                                config.write(configfile)
 
-        elif last_season > season:
-            write_to_requester_log("Wrong config, Please check")
+        except:
+            traceback.print_exc()
 
-    write_to_requester_log("\n\n")
-    write_to_requester_log("*" * 20)
-    write_to_requester_log("Process finished, exiting....")
-    write_to_requester_log(("*" * 20) + "\n\n")
 
+def get_series_url(name):
+    name = name.replace(" ", "+")
+    name = name.replace("_", "+")
+    search_url = '/?keyword=%s&search_section=1'
+    url = base_url + search_url % name
+    write_to_requester_log(url, False)
+    search_request = m_requests.get(url)
+    search_page = html.fromstring(search_request.text)
+    result = search_page.xpath('/html/body/div[1]/div[2]/div[2]/a')[0].values()[0]
+    result = result.replace("watch", "tv")
+    return result
 
 if __name__ == '__main__':
     check()
